b9dcda67e164c7ace4c55ef2a55f4e222184e2e9
Thomas Degry <thomas@istrategylabs.com>
2014-02-27 11:09:17 -0500

    Fixed full screen video on android, placed stop button on bottom

--- static/js/mobile.js
+++ static/js/mobile.js
@@ -14,6 +14,33 @@
         $(".video-container").removeClass('hide');
         console.log('ready to call');
         console.log('joining room isl');
+
+        /* full screen video */
+        var width = $(window).width();
+        var height = $(window).height();
+
+        var currentWidth = $('video').width();
+        var currentHeight = $('video').height();
+
+        var containerEq = width / height;
+
+        var videoEq = currentWidth / currentHeight;
+
+        console.log(containerEq, videoEq);
+
+        var vw = width;
+        var vh = height;
+
+        if(containerEq <= videoEq){
+            vw = height * videoEq;
+            vh = height;
+        }else{
+            vw = width;
+            vh = width/videoEq;
+        }
+
+        $("video").css({'width':vw,"height":vh});
+
         return webrtc.joinRoom('isl');
     });
 
